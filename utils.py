fa97dce4137c546e3f156b76e3b1973a23c6f9b2
Tommaso Grandi <118896276+tommasograndi@users.noreply.github.com>
2023-12-20 10:18:07 +0100

    Update notebook

--- utils.py
+++ utils.py
@@ -2,9 +2,17 @@
 import pandas as pd 
 import numpy as np
 
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+
+# REALIZED VOLATILITY
+def realized_VOL(series):
+    vol = pd.Series(series.rolling(window=22).std().shift(-21))
+
+    return vol
 
 def realized_vol(series):
-    vol = pd.Series(series.rolling(window=25).std().shift(-24))
+    vol = pd.Series(series.rolling(window=22).std().shift(-21))
 
     return vol
 
@@ -39,3 +47,24 @@ def GarmanKlass(data, sd=True):
     
     # return the series
     return pd.Series(np.sqrt(x), index = data.index) 
+
+
+# models : dictionary with key the name of the model and value the forecast of the model (or the fit)
+# realized : the observed daily volatilities
+def evaluate(models : dict, realized): 
+
+    results_df = {}
+
+    for forecast in models.values():
+
+        # Call sklearn function to calculate MAE
+        results_df['MAE'] = mean_absolute_error(realized, forecast)
+
+        # Call sklearn function to calculate MSE
+        results_df['MSE'] = mean_squared_error(realized, forecast)
+
+
+    results_df = pd.DataFrame(results_df, index = models.keys())
+
+    return results_df
+
